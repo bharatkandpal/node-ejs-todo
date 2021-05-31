f8692cad8fe0c24c25e5b44a33bdc898bdff7f91
bharatkandpal <36111716+bharatkandpal@users.noreply.github.com>
2021-05-31 23:56:07 +0530

    Updated server.js for custom routes

--- server.js
+++ server.js
@@ -1,44 +1,84 @@
 
 const express = require("express")
-const date=require(__dirname+"/date.js")
+const mongoose = require("mongoose")
+const date = require(__dirname + "/date.js")
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 
-let items = []
-let workItems = []
+mongoose.connect("mongodb://localhost:27017/todoDB", { useNewUrlParser: true })
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type: String
+})
+
+const Item = mongoose.model("Item", itemSchema)
+
+// const itemList = ["work","study","sleep"].map(item=> new Item({name:item,type:"new"}))
+
+// Item.insertMany(itemList)
+// .then(()=>console.log("whoa..!! great work"))
+// .catch(e=>console.log(e.message))
+// Item.findByIdAndDelete("60b5214f5dd6942af49332c4")
+// .then(()=>console.log("deleted"))
+// .catch(e=>console.log(e.message))
+
 app.get("/", (req, res) => {
-    let day=date.getDay()
-    res.render("list", { listTitle: day, items: items })
+    let day = date.getDay()
+    Item.find()
+    // .then(items=>items.map(i=>i.name))
+    .then(itemNames=>itemNames.length>0 && res.render("list", { listTitle: day, items: itemNames }))
+    
 })
 
-app.get("/work", (req, res) => {
-    res.render("list", { listTitle: "Work List", items: workItems })
+
+
+app.get("/:customRoute", (req, res) => {
+    Item.find({type:req.params.customRoute})
+    .then((data)=>res.render("list", { listTitle: req.params.customRoute, items: data }))
 })
 
-app.get("/test",(req,res)=>{
+app.post("/delete",(req,res)=>{
+    Item.findByIdAndDelete(req.body.chkItem)
+    .then(()=>res.redirect("/"+req.body.listName))
+    .catch(e=>console.log(e.message))
+    
+})
+
+app.post("/:customRoute", (req, res) => {
+    console.log(req.body)
+    let item = new Item({
+        name: req.body.item,
+        type: req.params.customRoute
+    })
+    item.save()
+    .then(()=>res.redirect("/"+req.params.customRoute))
+    .catch(e=>console.log(e))
+})
+app.get("/:customRoute", (req, res) => {
+    Item.find({type:req.params.customRoute})
+    .then((data)=>res.render("list", { listTitle: req.params.customRoute, items: data }))
+})
+
+app.get("/test", (req, res) => {
     res.render("test")
 })
 
-app.post("/work", (req, res) => {
-    workItems.push(req.body.item)
-    
-    res.redirect("/work")
-})
-
 app.post("/", (req, res) => {
-    var item = req.body.item;
-    console.log(req.body);
-    if (req.body.btnAdd === "Work") {
-        workItems.push(item)
-        res.redirect("/work")
-    }
-    else {
-        items.push(item)
-        res.redirect("/")
-    }
+    let item = new Item({
+        name: req.body.item,
+        type: "home"
+    })
+    item.save()
+    .then(()=>res.redirect("/"))
+    .catch(e=>console.log(e))
+    
 })
 
 app.listen("3000", () => {
